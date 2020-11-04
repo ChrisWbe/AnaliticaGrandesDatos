867d753f38283167967cc7802ff5d5bd69185ba0
Christian Quintero Arias <chriswbe1993@gmail.com>
2020-11-03 20:48:48 -0500

    01-hadoop-50/q02-10 - Resultado

--- 01-hadoop-50/q02-10/mapper.py
+++ 01-hadoop-50/q02-10/mapper.py
@@ -1,4 +1,14 @@
+#! /usr/bin/python3
 import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+
+
+    for line in sys.stdin:
+
+        arrayWord = []
+        for word in line.split(','):
+            arrayWord.append(word)
+        sys.stdout.write("{}\t{}\n".format(arrayWord[3],arrayWord[4]))
