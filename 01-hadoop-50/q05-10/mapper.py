fa488eb14c616952d33a2e23cc7c993186bbe3a0
Christian Quintero Arias <chriswbe1993@gmail.com>
2020-11-03 22:21:12 -0500

    01-hadoop-50/q05-10 - Completo

--- 01-hadoop-50/q05-10/mapper.py
+++ 01-hadoop-50/q05-10/mapper.py
@@ -1,4 +1,18 @@
+#! /usr/bin/python3
+
 import sys
 #
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+ 
+if __name__ == '__main__':
+    rows=0
+    for line in sys.stdin:
+        
+        arrayWord = []
+        
+        for word in line.split('  '):
+            arrayWord.append(word)
+        ano, mes, dia = arrayWord[1].split('-')
+        sys.stdout.write("{}\t1\n".format(mes))
